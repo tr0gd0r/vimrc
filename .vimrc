93c718a58a5cf98345292670c4b009d14c999f71
tr0gd0r <anton.dorfman@rochester.edu>
2016-01-17 15:42:48 -0500

    wip

---
+++ .vimrc
@@ -0,0 +1,6 @@
+set number
+set mouse=a
+
+" colorscheme
+syntax on
+colorscheme buddy
---
+++ setup_vim.sh
@@ -0,0 +1,5 @@
+#install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle 
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+curl -LSso ~/.vimrc https://raw.githubusercontent.com/tr0gd0r/vimrc/master/.vimrc
