17d23180bda2cd06bef289b55734444e833ee2fd
tr0gd0r <anton.dorfman@rochester.edu>
2016-01-17 15:54:08 -0500

    more stuff

--- .vimrc
+++ .vimrc
@@ -1,6 +1,11 @@
+execute pathogen#infect()
 set number
 set mouse=a
 
 " colorscheme
 syntax on
 colorscheme buddy
+
+" lightline.vim
+set noshowmode
+set laststatus=
--- setup_vim.sh
+++ setup_vim.sh
@@ -1,5 +1,13 @@
-#install pathogen
+set -x
+
+# install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle 
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+# download vimrc
 curl -LSso ~/.vimrc https://raw.githubusercontent.com/tr0gd0r/vimrc/master/.vimrc
+
+# download plugins
+git clone -q https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline.vim
+mkdir -p ~/.vim/colors
+curl -LSso ~/.vim/colors/buddy.vim https://raw.githubusercontent.com/DrSpatula/vim-buddy/master/colors/buddy.vim
